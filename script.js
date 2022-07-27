962a60212e1ef72109ec3bb1652a9be880f7c1dc
Griffin <griffinquarles11@gmail.com>
2022-07-27 13:30:43 -0500

    Everything but remove button on

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
-let myLibrary = [];
+
+const addBookBtn = document.getElementById("addBookBtn");
+const popup = document.getElementById("content");
+let titleInput = document.querySelector("#title");
+let authorInput = document.querySelector("#author");
+let pagesInput = document.querySelector("#pages");
+let haveReadInput = document.querySelector("#haveRead");
+let bookshelf = document.querySelector("#bookshelf");
+const cancelBtn = document.getElementById("cancelBookBtn");
+const confirmBtn = document.getElementById("confirmBookBtn");
+let form = document.querySelector("formContainer");
+
 
 function Book(title,author,pages,read){
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info =function(){
-    return `${title} by ${author}, ${pages} pages, ${read}`
-  }
 }
+let myLibrary = [];
+
 function addBookToLibrary(){
-  
+    
+    title = titleInput.value;
+    author = authorInput.value;
+    pages = pagesInput.value;
+    if(haveReadInput.checked){
+         read = "Have Read";
+    }else{
+        read = "Have Not Read";
+    }
+
+    let newBook = new Book(title, author, pages,read);
+    myLibrary.push(newBook);
+    closeForm();
+    let card = document.createElement('div');
+    card.className = "card";
+    let titleSpan = document.createElement('div');
+    titleSpan.innerHTML = title.bold();
+    titleSpan.className = "cardInfo";
+    card.append(titleSpan);
+    let authorSpan = document.createElement('div');
+    authorSpan.innerHTML = author;
+    authorSpan.className = "cardInfo";
+    card.append(authorSpan);
+    let pageSpan = document.createElement('div');
+    pageSpan.innerHTML = `${pages} pages`;
+    pageSpan.className = "cardInfo";
+    card.append(pageSpan);
+    let readDiv = document.createElement('div');
+    readDiv.innerHTML= read;
+    if(readDiv.innerHTML == "Have Read"){
+        readDiv.className = "readDiv";
+    }else{
+        readDiv.className = "notReadDiv";
+    }
+    card.append(readDiv);
+    bookshelf.append(card);
 }
+function openForm(){
+    popup.style.display = "block";
+}
+function closeForm(){
+    popup.style.display = 'none';
+    titleInput.value= "";
+    authorInput.value="";
+    pagesInput.value="";
+    haveReadInput.checked = false;
+}
+
+
 const meditations = new Book("Meditations", "Marcus Aurelius",187,"Read")
-console.log(meditations.info());
+
