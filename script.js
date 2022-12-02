409229d8a2ee99087db4e3f6c40690742dbcd72a
Griffin <griffinquarles11@gmail.com>
2022-12-02 12:11:21 -0600

    refactor constructor to class

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ const confirmBtn = document.getElementById("confirmBookBtn");
 let form = document.querySelector("formContainer");
 
 
-function Book(title,author,pages,read){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book{
+    constructor (title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 }
 let myLibrary = [];
 
